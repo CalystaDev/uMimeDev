6fc249bec75b4ae29dcccb2f414a759c97885856
Ria Patel <ria2004.patel@gmail.com>
2024-11-19 10:12:32 -0500

    changed the port configs, made it clear to be flask app, and had an import error with np and cv

--- backend/video-gen-api/api.py
+++ backend/video-gen-api/api.py
@@ -251,4 +251,4 @@ def create_video(video_id):
     return jsonify({"message": "Video created successfully", "video_file": final_video_file}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
