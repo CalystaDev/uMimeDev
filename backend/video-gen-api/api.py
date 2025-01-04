2153f45f43cbc3e7f0b34b6d9ceb9500be864264
ssrivastava22 <saniya.d22@gmail.com>
2025-01-04 18:42:45 -0500

    removed comments

--- backend/video-gen-api/api.py
+++ backend/video-gen-api/api.py
@@ -76,26 +76,6 @@ def generate_script_from_llm(prompt: str) -> Tuple[List[str], str, str, str]:
     except Exception as e:
         return [], f"An error occurred: {str(e)}", "", ""
 
-# def generate_images_from_script(image_prompts: List[str], video_id: str) -> List[str]:
-#     image_urls = []
-#     openai.api_key = open_ai_api_key
-#     for i, prompt in enumerate(image_prompts):
-#         try:
-#             response = openai.Image.create(
-#                 prompt=prompt,
-#                 n=1,
-#                 size="512x512"
-#             )
-#             image_url = response['data'][0]['url']
-#             local_image_path = f"image_{video_id}_{i}.png"
-#             with open(local_image_path, 'wb') as img_file:
-#                 img_file.write(requests.get(image_url).content)
-#             gcs_image_url = upload_to_gcs(local_image_path, BUCKET_NAME, f"{video_id}/images/image_{i}.png")
-#             image_urls.append(gcs_image_url)
-#         except Exception as e:
-#             print(f"Error generating image for prompt '{prompt}': {e}")
-#     return image_urls
-
 
 # Lock to prevent race conditions during GCS uploads
 gcs_lock = threading.Lock()
@@ -177,44 +157,6 @@ def generate_audio(script: str, script_with_time_delimiter: str, voice_id: str, 
         else:
             word_count += 1
     return gcs_audio_url, response_dict, words
-
-# def create_video_with_audio(video_path: str, image_urls: list, words: list, audio_url: str, video_id: str):
-#     bottom_clip = VideoFileClip(video_path)
-#     bottom_clip = bottom_clip.resize(height=1920)
-#     bottom_clip = bottom_clip.crop(x_center=bottom_clip.w / 2, width=1080)
-#     width, height = bottom_clip.size
-#     bottom_half_clip = bottom_clip.crop(y1=height / 2 - 150, y2=height - 150)
-
-#     image_clips = []
-#     image_end_times = [words[i - 1][2] + 1 for i, word in enumerate(words) if word[0] == "$"]
-
-#     for i, image_url in enumerate(image_urls):
-#         img_clip = ImageClip(image_url).resize(height=1920/2).crop(x_center=bottom_clip.w/2, width=1080)
-#         img_clip = img_clip.set_start(0 if i == 0 else image_end_times[i - 1]).set_end(image_end_times[i] if i != len(image_end_times) - 1 else bottom_clip.duration)
-#         img_clip = img_clip.resize(width=width)
-#         ken_burns_clip = img_clip.fx(resize, lambda t: 1 + 0.02 * t)
-#         cropped_image_clip = ken_burns_clip.crop(y2=height / 2).set_position(("center", "top"))
-#         image_clips.append(cropped_image_clip)
-
-#     top_half_clip = concatenate_videoclips(image_clips, method="compose", padding=-0.2)
-#     final_clip = CompositeVideoClip([top_half_clip, bottom_half_clip.set_position(("center", "bottom"))], size=(width, height))
-#     audio_clip = AudioFileClip(audio_url)
-#     final_clip = final_clip.set_duration(audio_clip.duration)
-#     final_clip = final_clip.set_audio(audio_clip)
-#     caption_clips = []
-#     for text, start, end in words:
-#         if text == '$':
-#             continue
-#         word_duration = (end - start)
-#         word_clip = TextClip(text, fontsize=100, color='white', stroke_color='black', stroke_width=2, font='Impact', align='center')
-#         word_clip = word_clip.set_position(('center', 'center')).set_start(start).set_duration(word_duration).crossfadein(0.1)
-#         caption_clips.append(word_clip)
-#     final_video_with_captions = CompositeVideoClip([final_clip] + caption_clips)
-#     output_file = f"final_video_with_audio_{video_id}.mp4"
-#     final_video_with_captions.write_videofile(output_file, audio_codec="aac")
-#     # Upload final video to GCS
-#     gcs_video_url = upload_to_gcs(output_file, BUCKET_NAME, f"{video_id}/final_video.mp4")
-#     return gcs_video_url
 
 def read_image_from_url(url):
     response = requests.get(url, stream=True)
